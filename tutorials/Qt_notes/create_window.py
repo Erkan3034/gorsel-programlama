d4a725adba9b167d0e4179c79613db2c5cc5ed7d
Erkan3034 <turguterkan55@gmail.com>
2025-10-09 15:50:25 +0300

    refactor: update window creation functions and add docstring

--- tutorials/Qt_notes/create_window.py
+++ tutorials/Qt_notes/create_window.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5  import QtWidgets
-
+"""
 def window():
     app = QtWidgets.QApplication(sys.argv) # Create an instance of QApplication 
     window= QtWidgets.QWidget() # Create an instance of QWidget
@@ -10,3 +10,16 @@ def window():
     sys.exit(app.exec_()) # Start the application's event loop
 
 window()
+
+"""
+
+def create_a_window():
+    app =QtWidgets.QApplication(sys.argv) #uygulama ornegi olustur
+    window= QtWidgets.QWidget() #pencere ornegi olustur
+    window.setWindowTitle("İlk GUI projem") #pencere basligini ayarla
+    window.setGeometry(250,250,400,400) #pencerenin konumunu ve boyutunu ayarla (x_pos, y_pos, width, height)
+    window.show() #pencereyi goster
+    sys.exit(app.exec_()) #uygulamanin olay dongusunu baslat
+
+   
+create_a_window()
