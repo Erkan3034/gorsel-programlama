029d4fa0bf32dafdde7f2ce54e72b44ce24f94b7
Erkan3034 <turguterkan55@gmail.com>
2025-10-23 22:48:14 +0300

    refactor: update example3.py to include INFO prefix in output messages for triangle classification

--- tutorials/classNotes/example3.py
+++ tutorials/classNotes/example3.py
@@ -7,12 +7,14 @@ b=int(input("2. Kenar Uzunluğunu Giriniz: "))
 c=int(input("3. Kenar Uzunluğunu Giriniz: "))
 
 if a==b and b==c:
-    print("Eşkenar Üçgen")
+    print("INFO: Eşkenar Üçgen")
 elif a==b or b==c or a==c:
-    print("İkizkenar Üçgen")    
+    print("INFO: İkizkenar Üçgen")    
 elif a+b>c and a+c>b and b+c>a:
     print("INFO: Üçgendir!")
 else:
-    print("Üçgen Değildir!")    
-print("Program Finished...\n\n")
+    print("INFO: Üçgen Değildir!")    
+print("INFO: Program Finished...\n\n")
 # End of the program
+
+
