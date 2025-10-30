3c3a6fd919443d64d84e2aab55b09d2d6386160b
Erkan3034 <turguterkan55@gmail.com>
2025-10-30 15:20:30 +0300

    feat: add while loop examples for basic iteration demonstration

---
+++ tutorials/classNotes/while.py
@@ -0,0 +1,16 @@
+sayac =1
+
+while sayac <6:
+    print("Merhaba Dünya")
+    sayac +=1
+
+print("\nINFO: Merhaba dünya döngüsü bitti\n")
+
+
+#===============================
+
+i =1
+while i <6:
+    print(i)
+    i +=1
+print("\nINFO: Artırma döngüsü bitti\n")
