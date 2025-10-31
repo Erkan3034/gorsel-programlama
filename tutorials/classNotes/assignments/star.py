e5d5128758e4500a631c894e9e46a4436999923c
Erkan3034 <turguterkan55@gmail.com>
2025-10-31 23:28:51 +0300

    feat: update star.py to implement a star pyramid pattern with both increasing and decreasing sections based on user input

--- tutorials/classNotes/assignments/star.py
+++ tutorials/classNotes/assignments/star.py
@@ -2,24 +2,28 @@
 Yıldız Piramit Uygulaması
 """
 
-n = int(input("Piramidin yüksekliğini giriniz: "))
+n = int(input("Piramidin maksimum yıldız sayısını giriniz: "))
 
+# Artan kısım (1'den n'e kadar)
 i = 1
 while i <= n:
-    # Boşlukları yazdır
+    
     j = 1
-    while j <= n - i:
-        print(" ", end="")
-        j += 1
-    
-    # Yıldızları yazdır
-    k = 1
-    while k <= (2 * i - 1):
+    while j <= i:
         print("*", end="")
-        k += 1
-    
+        j += 1
     print()  # Satır sonu
     i += 1
+
+# Azalan kısım (n-1'den 1'e kadar)
+i = n - 1
+while i >= 1:
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()  # Satır sonu
+    i -= 1
 
 print("\nINFO: Program Finished...\n\n")
 # End of the program
