2017976ac307c13fea59c3d4609c5934006ea8d7
Erkan3034 <turguterkan55@gmail.com>
2025-10-31 23:33:48 +0300

    feat: modify star.py to allow repeated pyramid generation based on user input, maintaining both increasing and decreasing star patterns

--- tutorials/classNotes/assignments/star.py
+++ tutorials/classNotes/assignments/star.py
@@ -3,27 +3,32 @@ Yıldız Piramit Uygulaması
 """
 
 n = int(input("Piramidin maksimum yıldız sayısını giriniz: "))
+miktar = int(input("Piramidin kaç kez tekrarlanacağını giriniz: "))
 
-# Artan kısım (1'den n'e kadar)
-i = 1
-while i <= n:
+# Dış döngü: piramidin kaç kez tekrarlanacağı
+sayac = 1
+while True:
+    # Artan kısım (1'den n'e kadar)
+    i = 1
+    while i <= n:
+        j = 1
+        while j <= i:
+            print("*", end="")
+            j += 1
+        print()  # Satır sonu
+        i += 1
     
-    j = 1
-    while j <= i:
-        print("*", end="")
-        j += 1
-    print()  # Satır sonu
-    i += 1
-
-# Azalan kısım (n-1'den 1'e kadar)
-i = n - 1
-while i >= 1:
-    j = 1
-    while j <= i:
-        print("*", end="")
-        j += 1
-    print()  # Satır sonu
-    i -= 1
+    # Azalan kısım (n-1'den 1'e kadar)
+    i = n - 1
+    while i >= 1:
+        j = 1
+        while j <= i:
+            print("*", end="")
+            j += 1
+        print()  # Satır sonu
+        i -= 1
+    
+    sayac += 1
 
 print("\nINFO: Program Finished...\n\n")
 # End of the program
