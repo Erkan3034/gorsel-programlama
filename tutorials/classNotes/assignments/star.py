5b2ad76122e1d1a3b23dfa5cdf694950c62f6fdc
Erkan3034 <turguterkan55@gmail.com>
2025-11-05 16:42:30 +0300

    refactor: update comments in star.py to use Turkish language for better localization

--- tutorials/classNotes/assignments/star.py
+++ tutorials/classNotes/assignments/star.py
@@ -1,17 +1,17 @@
 import time
 
 def print_star_pattern(n):
-    # Increasing pattern
+    # artan desen
     for i in range(1, n + 1):
         print("* " * i)
         time.sleep(0.1)
     
-    # Decreasing pattern
-    for i in range(n - 1, 0, -1):
+    # azalan desen
+    for i in range(n - 1, 0, -1): #n-1 den 1 e kadar azalan
         print("* " * i)
         time.sleep(0.1)
     
-# Get input from user
+# kullanıcıdan girdi al
 n = int(input("Yıldız satırı uzunluğunu giriniz: "))
 
 while True:
