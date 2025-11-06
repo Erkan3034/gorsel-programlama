e186d385a16dd6cd9ef965f269175e87f09d9cf2
Erkan3034 <turguterkan55@gmail.com>
2025-11-06 12:55:56 +0300

    refactor: remove unnecessary blank lines in star.py for improved readability

--- tutorials/classNotes/assignments/star.py
+++ tutorials/classNotes/assignments/star.py
@@ -5,12 +5,10 @@ def print_star_pattern(n):
     for i in range(1, n + 1):
         print("* " * i)
         time.sleep(0.1)
-    
     # azalan desen
     for i in range(n - 1, 0, -1): #n-1 den 1 e kadar azalan
         print("* " * i)
         time.sleep(0.1)
-    
 # kullanıcıdan girdi al
 n = int(input("Yıldız satırı uzunluğunu giriniz: "))
 
