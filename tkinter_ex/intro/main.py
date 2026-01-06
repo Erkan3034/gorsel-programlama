57c4261fbbb27e565647b1cbd195b2776f73d204
Erkan3034 <turguterkan55@gmail.com>
2026-01-06 18:26:51 +0300

    added

--- tkinter_ex/intro/main.py
+++ tkinter_ex/intro/main.py
@@ -1,1 +1,20 @@
-import tkinter as tk
+import tkinter as tk 
+from tkinter import Label
+
+root = tk.Tk() # pencere olustur
+root.title("İlk Tkinter Uygulamam") # pencere basligi
+root.geometry("400x300") # pencere boyutu
+root.resizable(False, False) # pencere boyutunu degistirme
+
+
+helloText = Label(root, text="Hello World", font=("Arial", 16), fg="red")
+helloText.pack()
+
+
+
+root.mainloop() # ana donguyu baslat
+
+
+
+def helloUser():
+    
