85bcbabb13b8d6be2a2783b2e6299df57fdaf7f3
Erkan3034 <turguterkan55@gmail.com>
2026-01-06 18:37:02 +0300

    updated main.py from tkinter

--- tkinter_ex/intro/main.py
+++ tkinter_ex/intro/main.py
@@ -1,20 +1,38 @@
 import tkinter as tk 
-from tkinter import Label
+from tkinter import Label, Entry, Button
 
 root = tk.Tk() # pencere olustur
 root.title("İlk Tkinter Uygulamam") # pencere basligi
 root.geometry("400x300") # pencere boyutu
-root.resizable(False, False) # pencere boyutunu degistirme
+root.resizable(True, True) # pencere boyutunu degistirme
 
 
-helloText = Label(root, text="Hello World", font=("Arial", 16), fg="red")
-helloText.pack()
+helloText = Label(root, text="Hello World", justify="center", font=("Arial", 16), fg="red")
+helloText.pack(pady=20, padx=20) #
+
+# Kullanicidan veri almak icin Entry
+nameLabel = Label(root, text="Adinizi giriniz:", font=("Arial", 12))
+nameLabel.pack(pady=10, padx=10)
+
+nameEntry = Entry(root, justify="center", font=("Arial", 12))
+nameEntry.pack(pady=10, padx=10)
 
 
+# Soy isim
+lastNameLabel = Label(root, text="Soyadinizi giriniz:", font=("Arial", 12))
+lastNameLabel.pack(pady=10, padx=10)
 
-root.mainloop() # ana donguyu baslat
-
+lastNameEntry = Entry(root, justify="center", font=("Arial", 12))
+lastNameEntry.pack(pady=10, padx=10)
 
 
 def helloUser():
-    
+    name = nameEntry.get() # Entry icindeki veriyi al
+    lastName = lastNameEntry.get()
+    helloText.config(text=f"Hello, {name} {lastName}") # Label'i guncelle
+
+# Buton
+btn = Button(root, text="Gonder", command=helloUser)
+btn.pack(pady=10, padx=10)
+
+root.mainloop() # ana donguyu baslat
